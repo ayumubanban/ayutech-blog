fca714a755d95e19f8114a750dfe437e74afaf9d
ayumu <ayumuban@outlook.com>
2019-09-09 13:01:25 +0900

    fixOGP2

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Pandas Eating Lots`,
     description: `ayumu's technical blog`,
     author: `ayumu`,
-    ogpImg: `https://images.unsplash.com/photo-1542359649-31e03cd4d909?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2767&q=80`,
+    ogpImg: `https://source.unsplash.com/collection/8598671`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
